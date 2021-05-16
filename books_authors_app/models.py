60bda5becc3d7e2bae89e3590166049e3b76c53d
DEBORAH LONG <sb.long@yahoo.com>
2021-05-16 13:41:01 -0700

    first commit

--- books_authors_app/models.py
+++ books_authors_app/models.py
@@ -1,18 +1,20 @@
 from django.db import models
 
 # Create your models here.
-class books(models.Model):
-    title = models.CharField(max_length = 255)
-    desc = models.TextField()
-    created_at = models.DateTimeField(auto_now_add = True)
-    updated_at = models.DateTimeField(auto_now = True)
+class Book(models.Model):
+    title = models.CharField(max_length=45)
+    description = models.CharField(max_length=255)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
-class authors(models.Model):
-    first_name = models.CharField(max_length = 45)
-    last_name = models.CharField(max_length = 45)
-    notes = models.TextField()
-    books = models.ManyToManyField(books, related_name = 'authors')
-    created_at = models.DateTimeField(auto_now_add = True)
-    updated_at = models.DateTimeField(auto_now = True)
+class Author(models.Model):
+    first_name = models.CharField(max_length=45)
+    last_name = models.CharField(max_length=45)
+    notes = models.CharField(max_length=255)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    books = models.ManyToManyField('Book', related_name='authors')
 
-book1 = books.objects.create(title='C Sharp')
+    @property
+    def full_name(self):
+        return f"{self.first_name} {self.last_name}"
